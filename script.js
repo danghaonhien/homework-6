b8ae504db401682a576bbd10f158c70ed5ebd3bd
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-12 23:20:32 -0800

    add temp,hour

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+    var day = moment().format('MMM Do YY');
+$("#realTime").text(day)
+let time = moment().format("hh:mm a");
+$("#time").text(time);
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
     // Here we are building the URL we need to query the database
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Berkeley&units=imperial&appid=" + APIKey;
@@ -11,12 +16,14 @@ var APIKey = "166a433c57516f51dfab1f7edaed8413";
       console.log(response);      
       // Create CODE HERE to log the resulting object
       // Create CODE HERE to transfer content to HTML
-      $('.city').text(`${response.name} Weather details`);
+      $('.city').text(`${response.name}`);
       $('.wind').text(`Wind speed: ${response.wind.speed}`);
       $('.humidity').text(`Humidity: ${response.main.humidity}`);
       // Create CODE HERE to calculate the temperature (converted from Kelvin)
-      const f = (response.main.temp - 273.15) * 1.8 + 32;
+    //   const f = (response.main.temp - 273.15) * 1.8 + 32;
       // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-      $('.temp').text(`Temp in Fahrenheit: ${f.toFixed(2)}`);
+      $('.temp').text(response.main.temp + " F ");
       // Create CODE HERE to dump the temperature content into HTML
     });
+
+})
