e67d9f1b56c0aa5e75b23b29ee9c33ce8b01e637
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-12 22:37:19 -0800

    add script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
     // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Berkeley&units=imperial&appid=" + APIKey;
     // We then created an AJAX call
     $.ajax({
       url: queryURL,
